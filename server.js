ed5a2db5d566121b841b5d3892e4a59403ea1181
rcpang <rcpang@UNC-R90YDFW2.localdomain>
2022-11-17 16:19:08 -0500

    1st submission

---
+++ server.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+import { roll } from "./lib/lib/roll.js";
+import express from "express";
+import minimist from "minimist";
+
+const app = express();
+const args = minimist(process.argv.slice(2));
+
+app.use(express.urlencoded({extended: true}));
+app.get('/app/', (req, res) => {
+	res.send('200 OK');
+});
+app.get('/app/roll/', (req, res) => {
+	res.send(roll(6, 2, 1))
+});
+
+app.post('/app/roll', (req, res) => {
+	const sides = parseInt(req.body.sides)
+	const dice = parseInt(req.body.dice)
+	const rolls = parseInt(req.body.rolls)
+	res.send(roll(sides, dice, rolls))
+});
+
+app.get('/app/roll/:sides/', (req, res) => {
+	const sides = parseInt(req.params.sides)
+	res.send(roll(sides, 2, 1))
+});
+app.get('/app/roll/:sides/:dice', (req, res) => {
+	const sides = parseInt(req.params.sides)
+	const dice = parseInt(req.params.dice)
+	res.send(roll(sides, dice, 1))
+});
+app.get('/app/roll/:sides/:dice/:rolls', (req, res) => {
+	const sides = parseInt(req.params.sides)
+	const dice = parseInt(req.paramas.dice)
+	const rolls = parseInt(req.params.rolls)
+	res.send(roll(sides, dice, rolls))
+});
+
+app.use((req, res) => {
+	res.status(404).send("404 NOT FOUND")
+});
+app.listen(args.port || 5000);
